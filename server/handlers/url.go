69184d9b507ce04b9f0a850410227bdf285018f1
jaisondavid-m <jaisondavidm.cs25@bitsathy.ac.in>
2026-04-27 13:03:41 +0530

    fixed same random code generating err

--- server/handlers/url.go
+++ server/handlers/url.go
@@ -15,35 +15,50 @@ func CreateShortURL(c *gin.Context) {
 	var req models.UrlReq
 
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest,gin.H{
-			"message":"Invalid Input",
-			"error":true,
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid Input",
+			"error":   true,
 		})
 		return
 	}
 
 	userID := c.GetUint("userID")
 
-	shortCode := utils.GenerateShortCode()
+	// shortCode := utils.GenerateShortCode()
+	var shortCode string
+
+	for {
+
+		shortCode = utils.GenerateShortCode()
+
+		var count int64
+
+		config.DB.Model(&models.URL{}).Where("short_code = ?", shortCode).Count(&count)
+
+		if count == 0 {
+			break
+		}
+
+	}
 
 	url := models.URL{
 		OriginalURL: req.OriginalURL,
-		ShortCode: shortCode,
-		UserID: userID,
+		ShortCode:   shortCode,
+		UserID:      userID,
 	}
 
 	if err := config.DB.Create(&url).Error; err != nil {
-		c.JSON(http.StatusInternalServerError,gin.H{
-			"message":"Unable to create URL",
-			"error":true,
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Unable to create URL",
+			"error":   true,
 		})
 		return
 	}
 
-	c.JSON(http.StatusOK,gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"short_url": "http://localhost:8000/" + shortCode,
-		"data": url,
-		"success":false,
+		"data":      url,
+		"success":   false,
 	})
 
 }
